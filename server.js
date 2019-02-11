842888c5e5cc7880b89f3ca720c2a3a2481271d0
Bikash Ruwali <bikashruwali@Bikashs-MBP.attlocal.net>
2019-02-10 23:41:50 -0600

    Added MongoDB to our app! KUDOS!

--- server.js
+++ server.js
@@ -3,26 +3,50 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-]
+var dbUrl = 'mongodb+srv://user:user@plushealth-iizho.mongodb.net/plushealth?ssl=true&authSource=admin'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({},(err, messages) => {
+        res.send(messages)
+    })
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message',req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body);
+
+    message.save()
+    .then( item => {
+        console.log("item saved to database")
+        messages.push(req.body)
+        io.emit('message',req.body)
+        res.sendStatus(200)
+    
+    })
+    .catch(err =>{
+        sendStatus(500)
+         
+    })  
+   
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected to our server')
+}) 
+
+mongoose.connect(dbUrl, {useNewUrlParser: true}, function(err, db){
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
