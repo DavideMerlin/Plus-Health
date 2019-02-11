1c6a0563bba6315de7258592106c9e81ee1acf73
Bikash Ruwali <bikashruwali@Bikashs-MBP.attlocal.net>
2019-02-10 18:32:07 -0600

    Added the chat inferface for plus health app!!

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
-app.listen(3000)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var messages = [
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    io.emit('message',req.body)
+    res.sendStatus(200)
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected to our server')
+})
+
+var server = http.listen(3000, () => {
+    console.log('server is listening on port', server.address().port)
+}) 
