1163f30765feaebe1b170d7a5c54880019c995b1
Bikash Ruwali <bikashruwali@Bikashs-MacBook-Pro.local>
2019-02-11 14:19:18 -0600

    fixed a bug in server.js

--- server.js
+++ server.js
@@ -1,53 +1,53 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var mongoose = require('mongoose')
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 var dbUrl = 'mongodb+srv://user:user@plushealth-iizho.mongodb.net/plushealth?ssl=true&authSource=admin'
 
-var Message = mongoose.model('Message', { 
+var Message = mongoose.model('Message', {
     name: String,
     message: String
-})
+});
 
 app.get('/messages', (req, res) => {
     Message.find({},(err, messages) => {
         res.send(messages)
     })
-})
+});
 
 app.post('/messages', (req, res) => {
     var message = new Message(req.body);
 
     message.save()
     .then( item => {
-        console.log("item saved to database")
+        console.log("item saved to database");
         
-        io.emit('message',req.body)
-        res.sendStatus(200)
+        io.emit('message',req.body);
+        res.sendStatus(200);
     
     })
-    .catch(err =>{
-        sendStatus(500)
+    .catch(err => {
+        sendStatus(500);
          
     })  
    
 })
 
 io.on('connection', (socket) => {
-    console.log('a user connected to our server')
+    console.log('a user connected to our server');
 }) 
 
 mongoose.connect(dbUrl, {useNewUrlParser: true}, function(err, db){
-    console.log('mongo db connection', err)
+    console.log('mongo db connection', err);
 })
 
 var server = http.listen(3000, () => {
-    console.log('server is listening on port', server.address().port)
-}) 
+    console.log('server is listening on port', server.address().port);
+}); 
